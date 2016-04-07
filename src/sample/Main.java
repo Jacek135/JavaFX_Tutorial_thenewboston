0245f1aac9b1dadad0ba45b015b06f6a2c9f63e8
Jacek135 <jac.poczatek@gmail.com>
2016-04-07 20:23:52 +0200

    End of 5rd tutorial

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 
     Stage window;
     Scene scene1, scene2;
+    Button button;
 
     public static void main(String[] args) {
         launch(args);
@@ -20,27 +21,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
+        window.setTitle("thenewboston");
 
-        Label label1 = new Label("Welcom to the first scene!");
-        Button button1 =  new Button("Go to scene 2");
-        button1.setOnAction(event -> window.setScene(scene2));
+        button = new Button("Click me");
+        button.setOnAction(event1 -> AlertBox.display("Title of window", "Wow this allert is awaseon!"));
 
-        //Layout 1 - children are lain out in vertical column
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 200, 200);
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
 
-        //Button 2
-        Button button2 =  new Button("This scene suck, go back to scene 1");
-        button2.setOnAction(event -> window.setScene(scene1));
-
-        // Layout 2
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2, 600, 300);
-
-        window.setScene(scene1);
-        window.setTitle("Title here");
+        window.setScene(scene);
         window.show();
     }
 }
