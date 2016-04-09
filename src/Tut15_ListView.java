4425cc88bfd01fcde276f29633c6c9b47124d04c
Jacek135 <jac.poczatek@gmail.com>
2016-04-09 17:01:49 +0200

    Tut15 - ListView

---
+++ src/Tut15_ListView.java
@@ -0,0 +1,52 @@
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Tut15_ListView extends Application {
+    Stage window;
+    Scene scene;
+    Button button;
+    ListView<String> listView;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("ChoiceBox Demo");
+        button = new Button("Submit");
+
+        listView = new ListView<>();
+        listView.getItems().addAll("Iron Man", "Titanic", "Contact", "Surrogates");
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        button.setOnAction(event -> buttonClicker());
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20, 20, 20, 20));
+        layout.getChildren().addAll(listView, button);
+
+        scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
+    }
+
+    private void buttonClicker(){
+        String message = "";
+        ObservableList<String> movies;
+        movies = listView.getSelectionModel().getSelectedItems();
+        for(String m: movies){
+            message += m + "\n";
+        }
+        System.out.println(message);
+    }
+
+}
